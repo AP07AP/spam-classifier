917f5d413323c5af379e6f87af1091b21e9c5d16
Aditya Prakash <136500500+AP07AP@users.noreply.github.com>
2024-12-22 19:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
 import streamlit as st
 import pickle
 import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.stem.porter import PorterStemmer
 
-# Link to the local nltk_data folder
-nltk.data.path.append('./nltk_data')
+# # Link to the local nltk_data folder
+# nltk.data.path.append('./nltk_data')
 
-ps = PorterStemmer()
-stop_words = set(stopwords.words('english'))  # Pre-load stopwords
+# ps = PorterStemmer()
+# stop_words = set(stopwords.words('english'))  # Pre-load stopwords
 
-def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
+# def transform_text(text):
+#     text = text.lower()
+#     text = nltk.word_tokenize(text)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+#     y = []
+#     for i in text:
+#         if i.isalnum():
+#             y.append(i)
 
-    text = y[:]
-    y.clear()
+#     text = y[:]
+#     y.clear()
 
-    for i in text:
-        if i not in stop_words and i not in string.punctuation:
-            y.append(i)
+#     for i in text:
+#         if i not in stop_words and i not in string.punctuation:
+#             y.append(i)
 
-    text = y[:]
-    y.clear()
+#     text = y[:]
+#     y.clear()
 
-    for i in text:
-        y.append(ps.stem(i))
+#     for i in text:
+#         y.append(ps.stem(i))
 
-    return " ".join(y)
+#     return " ".join(y)
 
 # Load model and vectorizer
 try:
